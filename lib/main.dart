0fffab1d6af8de4213a25bf1319c1ebddb43ff4f
AKIHIRO <a.kws.sports@gmail.com>
2025-11-24 17:49:48 +0900

    20251124 ログイン機能、登録機能、プロフィール一覧機能

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_options.dart';
+import 'profile_list_screen.dart';
+import 'login_screen.dart';
 
 Future<void> main() async {
-  // main関数はOKです
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
   runApp(const MyApp());
 }
 
-// MyAppクラスもOKです
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); // コンストラクタに super.key を追加すると良いでしょう
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,40 +24,27 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(), // constを外すか、MyHomePageのコンストラクタをconstにする
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // ロード中
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          // ログイン中
+          if (snapshot.hasData) {
+            return const ProfileListScreen();
+          }
+
+          // ログアウト状態
+          return const LoginScreen();
+        },
+      ),
     );
-  }
-}
-
-
-class MyHomePage extends StatelessWidget { // StatelessWidgetとして実装
-  const MyHomePage({super.key}); // コンストラクタを追加
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ログイン'), // タイトルを追加すると分かりやすいです
-      ), // AppBar
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0), // 少し余白を広げました
-            child: TextField(
-              onChanged: (text) {
-                // 入力された文字(text)を使って何か処理をする
-                print(text);
-              },
-              decoration: InputDecoration(
-                  labelText: "メールアドレス",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)))), //IputDecoration
-            ), //TextField
-          ) //Padding
-          // 他にもパスワード入力欄やボタンなどをここに追加できます
-        ],
-      ), // Column
-    ); //Sccafold
   }
 }
